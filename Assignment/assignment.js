6ae3cce7210d5da120715055d74c5d4e6ca95066
Austin Scrivener <ostim12121212@gmail.com>
2024-10-14 14:48:06 +1300

    Assignment started

---
+++ Assignment/assignment.js
@@ -0,0 +1,15 @@
+test = 10231231
+console.log(test)
+
+{
+    console.log(test)
+    test = 4000
+    console.log(test)
+}
+console.log(test)
+
+
+function page_head(){
+        object = document.getElementById("div")
+        console.log(object)
+    }
