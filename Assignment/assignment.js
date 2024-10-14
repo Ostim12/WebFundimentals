e44ebe38147776162807b068c2ff98af85e62b8d
Austin Scrivener <ostim12121212@gmail.com>
2024-10-14 20:51:47 +1300

    visuals done

--- Assignment/assignment.js
+++ Assignment/assignment.js
@@ -1,15 +1,14 @@
-test = 10231231
-console.log(test)
+function init() {
+    map = new OpenLayers.Map("ucolMap");
+    var mapnik         = new OpenLayers.Layer.OSM();
+    var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+    var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+    var position       = new OpenLayers.LonLat(175.61353382992954,-40.35221938661656).transform( fromProjection, toProjection);
+    var zoom           = 18; 
 
-{
-    console.log(test)
-    test = 4000
-    console.log(test)
+    map.addLayer(mapnik);
+    map.setCenter(position, zoom );
+    var markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers);
+    markers.addMarker(new OpenLayers.Marker(position));
 }
-console.log(test)
-
-
-function page_head(){
-        object = document.getElementById("div")
-        console.log(object)
-    }
