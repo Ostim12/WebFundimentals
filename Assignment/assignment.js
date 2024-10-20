b2bd986b17a432b260ecd4dcd364e9579329be09
Austin Scrivener <ostim12121212@gmail.com>
2024-10-21 10:47:31 +1300

    better map with an iframe

--- Assignment/assignment.js
+++ Assignment/assignment.js
@@ -1,22 +1,4 @@
 
-
-// map function
-function init() {
-    map = new OpenLayers.Map("ucolMap");
-    var mapnik         = new OpenLayers.Layer.OSM();
-    var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-    var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-    var position       = new OpenLayers.LonLat(175.61353382992954,-40.35221938661656).transform( fromProjection, toProjection);
-    var zoom           = 13; 
-
-    map.addLayer(mapnik);
-    map.setCenter(position, zoom );
-    var markers = new OpenLayers.Layer.Markers( "Markers" );
-    map.addLayer(markers);
-    markers.addMarker(new OpenLayers.Marker(position));
-}
-
-
 let handleMousemove = (event) => {
     let posX = event.x
     let posY = event.y
