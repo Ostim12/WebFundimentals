c18e35ac5fac0b700b3bf00792d339533beeae89
Austin Scrivener <ostim12121212@gmail.com>
2024-10-21 15:19:10 +1300

    event listeners added in js and removed from html

--- assignment.js
+++ assignment.js
@@ -1,3 +1,28 @@
+const btnDisplayXY =  document.getElementById("btn-display-x-y");
+const btnTheme = document.getElementById("btn-theme");
+const btnModal = document.getElementById("btn-modal");
+const btnSwapImage = document.getElementById("btn-swap-image");
+
+btnDisplayXY.addEventListener("mouseover", function() {
+    displayXY()
+});
+btnDisplayXY.addEventListener("mouseout", function() {
+    showDataTable()
+});
+btnDisplayXY.addEventListener("mousemove", function(event){
+    mousePos(event)
+})
+btnTheme.addEventListener("click", function() {
+    themeClick()
+});
+btnModal.addEventListener("click", function() {
+    modal()
+});
+btnSwapImage.addEventListener("click", function() {
+    swapImage()
+});
+
+
 function mousePos(event){
     let posX = event.clientX
     let posY = event.clientY
@@ -48,16 +73,22 @@ const imageLocation = document.getElementById("image").src
 let imageSource = imageLocation.split("business.jpg")[0]
 console.log(imageSource)
 
+function changeImage(object,imageName){
+    document.getElementById(object).src = (imageSource+imageName)
+}
+
 function swapImage(){
     console.log(document.getElementById("image").src)
     if (document.getElementById("image").src == (imageSource+"people.jpg")){
-        document.getElementById("image").src = (imageSource+"business.jpg")
-        document.getElementById("image2").src = (imageSource+"business.jpg")
+        changeImage("image","business.jpg")
+        changeImage("modal-image","business.jpg")
     }
     else{
         if(document.getElementById("image").src == (imageSource+"business.jpg")){
-            document.getElementById("image").src = (imageSource+"people.jpg")
-                document.getElementById("image2").src = (imageSource+"people.jpg")
+            changeImage("image","people.jpg")
+            changeImage("modal-image","people.jpg")
         }
     }
 }
+
+
